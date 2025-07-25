4c739dcdc1957f97e94738c0417b4a79b6701d43
Peter Fleming <fleming_peter01@student.smc.edu>
2025-07-24 17:04:06 -0700

    Initial commit - UserProfile component

---
+++ user-profile-lite/src/App.jsx
@@ -0,0 +1,18 @@
+import ContactCard from './ContactCard';
+import UserProfile from './UserProfile';
+
+function App() {
+  return (
+    <div>
+    <div>
+      <UserProfile />
+    </div>
+    <div>
+      <ContactCard />
+    </div>
+    </div>
+  );
+}
+
+
+export default App;
